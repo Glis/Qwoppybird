c81152d882e4930f3479514170e54503bf5aa067
Glis-hocs <jfuentes@stcsolutions.com.ve>
2014-02-21 10:37:27 -0430

    starting mech

---
+++ Assets/Scripts/FloorMove.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FloorMove : MonoBehaviour {
+
+	public float speed ;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (true) {
+			rigidbody2D.velocity = new Vector2(speed,0);
+		} else {
+			rigidbody2D.velocity = Vector2.zero;
+		}
+	}
+}
---
+++ Assets/Scripts/JumpingScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class JumpingScript : MonoBehaviour {
+
+	public KeyCode Jump;
+	public float speed;
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown(Jump)){
+			float velY = speed;
+			if(velY> 10f){
+				velY=10f;
+			}
+			rigidbody2D.velocity = new Vector2(0, velY);
+		}
+	}
+}
